e68f9ce176b45be4298284efcb7f5351019e89cc
meltphace7 <brd9326@protonmail.com>
2023-03-23 10:54:36 -0700

    converted most styles to tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "2rem",
+      "2xl": "2.5rem",
+      "3xl": "3rem",
+      "4xl": "3.5rem",
+      "5xl": "4rem",
+      "6xl": "6rem",
+      "7xl": "8rem",
+      "8xl": "10rem",
+      "9xl": "15rem",
+    },
     extend: {
       colors: {
         burgundyRed: " hsl(0, 60%, 24%)",
